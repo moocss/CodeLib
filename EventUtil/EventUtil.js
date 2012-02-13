6dc2ef6b709d2a27d568da86202f4bb42974bf97
f2er <uezheng@gmail.com>
2012-02-13 23:32:09 +0800

    add eventUtil

---
+++ EventUtil/EventUtil.js
@@ -0,0 +1,23 @@
+﻿/* *
+ * EventUtil
+ * 2012-02-13
+* */
+var EventUtil = EventUtil || {};
+EventUtil.addEvent = function(obj,otype,fn){
+	if( obj.addEventListener ){
+		obj.addEventListener( otype,fn,false );
+	}else if( obj.attachEvent ){
+		obj.attachEvent('on'+otype,fn);
+	}else{
+		obj['on'+otype ] = fn;
+	}
+} 
+EventUtil.removeEvent = function(obj,otype,fn){
+	if( obj.removeEventListener ){
+		obj.removeEventListener(otype,fn,false);
+	}else if( obj.detachEvent ){
+		obj.detachEvent('on'+type,fn);
+	}else{
+		obj['on'+otype]=null;
+	}
+}
---
+++ function/bind.js
@@ -0,0 +1,13 @@
+﻿/* *
+ * 函数绑定与函数柯里化
+ * 2012-02-13
+* */
+function bind( fn,context ){
+	var _context = context || window;
+	var args = Array.prototype.slice.call(arguments,2);
+	return function(){
+		var innerArgs = Array.prototype.slice.call(arguments);
+		var finalArgs = args.concat( innerArgs );
+		return fn.apply( _context,finalArgs );
+	}
+}
