ea75116bdb60b65bbc8a978a4b76cdeec34d858f
f2er <uezheng@gmail.com>
2012-02-14 22:23:33 +0800

    add throttle

---
+++ function/throttle.js
@@ -0,0 +1,26 @@
+﻿/* *
+ * 函数节流
+ * 2012-02-14
+* */
+
+//普通写法
+var processor = processor || {};
+processor = {
+	timeoutId : null,
+	process : function( fn ){
+		clearTimeout( this.timeoutId );
+		var that = this;
+		that.timeoutId = setTimeout(function(){
+			//判断参数类型
+			if( Object.prototype.toString.call(fn) == "[object Function]"){ fn(); }
+		},100)
+	}
+}
+
+//文艺写法
+function throttle(method,context){
+	clearTimeout( method.timeoutId );
+	method.timeoutId = setTimeout(function(){
+		method.call(context);
+	},100)
+}
