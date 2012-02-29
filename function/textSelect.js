c15bd993bbf2969270a18df8d0acbc53b618c4ad
f2er <uezheng@gmail.com>
2012-03-01 07:44:09 +0800

    add textSelect

---
+++ function/textSelect.js
@@ -0,0 +1,28 @@
+﻿/*
+  * setTextSelect : 选择部分文本
+  * getTextSelect : 设置部分文本
+  * 2012-03-01
+* */
+
+var textSelection = textSelection || {};
+textSelection = {
+    setTextSelect : function( obj,startIndex,stopIndex ){
+        if( obj.setSelectionRange ){
+            obj.setSelectionRange( startIndex,stopIndex );
+        }else if( obj.createTextRange ){
+            var _range = obj.createTextRange();
+            _range.collapse( true );
+            _range.moveStart( "character",startIndex );
+            _range.moveEnd( "character",stopIndex-startIndex );
+            _range.select();
+        }
+        obj.focus();
+    },
+    getTextSelect : function( obj ){
+        if( document.selection ){
+            return document.selection.createRange().text;
+        }else{
+            return obj.value.substring( obj.selectionStart,obj.selectionEnd );
+        }
+    }
+};
