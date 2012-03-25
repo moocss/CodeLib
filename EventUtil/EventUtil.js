6338c645abbfaf5b4006921a1870bf1a587abb97
f2er <uezheng@gmail.com>
2012-03-26 07:32:39 +0800

    update EventUtil

--- EventUtil/EventUtil.js
+++ EventUtil/EventUtil.js
@@ -149,3 +149,40 @@ Dom.offset = function( element ){
     };
     return _offset;
 };
+
+/*
+*  contains
+* */
+Dom.contains = function(refNode,otherNode){
+    if( typeof refNode.contains == "function"){
+        return refNode.contains(otherNode);
+    }else if( typeof refNode.compareDocumentPosition(otherNode)){
+        return !!(refNode.compareDocumentPosition(otherNode)&16);
+    }else{
+        var _node = otherNode.parentNode;
+        do{
+            if(_node === refNode){
+                return true;
+            }else{
+                _node = _node.parentNode;
+            }
+        }while(_node!=null)
+    }
+};
+
+Dom.InnerText = function(){
+    function getInnerText(element){
+        return ( typeof element.textContent == "string" ) ? element.textContent : element.innerText;
+    }
+    function setInnerText(element,text){
+        if( typeof element.textContent =="string" ){
+            element.textContent = text;
+        }else{
+            element.innerText = text;
+        }
+    }
+    return {
+        set : setInnerText,
+        get : getInnerText
+    }
+};
