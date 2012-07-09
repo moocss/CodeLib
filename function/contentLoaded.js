c9f78661f9e419aad967e3dc88cbc86eee05adf6
f2er <uezheng@gmail.com>
2012-07-09 19:40:33 +0800

    add contentLoaded

--- function/contentLoaded.js
+++ function/contentLoaded.js
@@ -1,7 +1,7 @@
 ﻿/* *
  * contentLoaded.js
  * 2012-07-09
-* */
+**/
 
 function contentLoaded(win,fn){
 	var done = false,
@@ -10,16 +10,16 @@ function contentLoaded(win,fn){
 		root = doc.documentElement,
 		
 		add = doc.addEventListener ? "addEventListener" : "attachEvent",
-		rem = doc.removeEventListener ? "removeEventListener" : "detachEvent",
-		pre = doc.addEventListener ? "" : "on";
+		rem = doc.addEventListener ? "removeEventListener" : "detachEvent",
+		pre = doc.addEventListener ? "" : "on",
 		
 		init = function(e){
 			if( e.type == 'readystatechange' && doc.readyState != 'complete'){
 				return;
 			}
 			( e.type == 'load' ? win : doc ) [rem](pre+e.type,init,false);
-			if( !done && (done == true) ){
-				fn.call( win,e.type || e );
+			if (!done && (done = true)) {
+				fn.call(win, e.type || e);
 			}
 		},
 		poll = function(){
@@ -32,20 +32,19 @@ function contentLoaded(win,fn){
 			init('poll');
 		};
 		
-		if( doc.readyState == 'complete'){
-			fn.call(win,'lazy');
-		}else{
-			if( doc.createEventObject && root.doScroll ){
-				try{
-					top = !win.frameElement;
-				}catch(e){
-				}
-				if( top ){
-					poll();
-				}
-				doc[add](pre+'DOMContentLoaded',init,false);
-				doc[add](pre+'readystatechange',init,false);
-				win[add](pre+'load',init,false);
+	if( doc.readyState == 'complete'){
+		fn.call(win,'lazy');
+	}else{
+		if( doc.createEventObject && root.doScroll ){
+			try{
+				top = !win.frameElement;
+			}catch(e){}
+			if( top ){
+				poll();
 			}
 		}
+		doc[add](pre + 'DOMContentLoaded', init, false);
+		doc[add](pre + 'readystatechange', init, false);
+		win[add](pre + 'load', init, false);
+	}
 }
