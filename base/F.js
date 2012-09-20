15026084ee3cbdf9fb1b386c3831a7f6eeb4ce98
f2er <uezheng@gmail.com>
2012-09-20 18:36:10 +0800

    update Q

--- base/F.js
+++ base/F.js
@@ -10,6 +10,7 @@
 	quickExpr = /^[^<]*(<[\w\W]+>)[^>]*$|^#([\w-]+)$/,
 	rsingleTag = /^<(\w+)\s*\/?>(?:<\/\1>)?$/,
 	toString = Object.prototype.toString;
+	
 	Q.fn = Q.prototype = {
 		init : function(selector,context){
 			var match,elem,ret,doc;
@@ -81,11 +82,24 @@
 		selector : "",
 		version : "0.1",
 		length : 0,
-		size : function(){}
+		size : function(){},
+		each : function(callback,args){
+			return Q.each(this,callback,args);
+		}
 	};
 	Q.extend = Q.fn.extend = function(){};
-	
 	Q.extend({
+		isPlainObject : function(obj){
+			if( !obj || toString.call(obj) != "[object Object]" || obj.nodeType || obj.setIntercal ){
+				return false;
+			}
+			if( obj.constructor && !hasOwnproperty.call(obj,"constructor") && !hasOwnproperty.call(obj.constructor.prototype,"isPrototypeOf") ){
+				return false;
+			}
+			var key;
+			for( key in obj ){}
+			return key == undefined || hasOwnproperty.call(obj,key);
+		},
 		isFunction : function(obj){
 			return toString.call(obj) == "[object Function]";
 		},
@@ -103,6 +117,54 @@
 				}
 			}
 			return -1;
+		},
+		each : function(object,callback,args){
+			var name,i=0,
+				length = object.length,
+				isObj = length === undefined || Q.isFunction(object);
+			if( args ){
+				if( isObj ){
+					for( name in object ){
+					
+					}
+				}else{
+					for(;i<length;){
+						if( callback.apply(object[i++],args) === false ){
+							break;
+						}
+					}
+				}
+			}else {
+				if( isObj ){
+					for( name in object ){
+						if( callback.call( object[name],name,object[name]) === false ){
+							break;
+						}
+					}
+				}else{
+					for( var value == object[0];i<length && callback.call(value,i,value) !== false;value =object[++i] ){
+						
+					}
+				}
+			}
+			 return object;
+		}
+	});
+	
+	Q.fn.extend({
+		attr : function(name,value){
+			return access(this,name,value,true,Q.attr);
+		},
+		removeAttr : function(name,fn){
+			return this.each(function(){
+				Q.attr( this,name,"");
+				if( this.nodeType ===1 ){
+					this.removeAttribute(name);
+				}
+			});
+		},
+		addClass : function(value){
+			
 		}
 	});
 	win.Q = win.$ = Q;
